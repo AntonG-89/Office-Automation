d8e54bf70e2d13349d64b2dc0c3e40895df70674
Anton Goncharenko <63752091+AntonG-89@users.noreply.github.com>
2020-08-09 01:20:58 -0400

    Update office_auto.py

--- office_auto.py
+++ office_auto.py
@@ -1,23 +1,28 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
 
 import os 
 from datetime import datetime
+import shutil
+
 today = datetime.now()
 wdir = os.chdir(r"C:\Users\anton\OneDrive\Desktop\Working folder")
+## Creating a new month folder/ accessing current month folder
 ##insert datetime month format as string instead of August
 if not os.path.exists('August'):
     os.makedirs('August')
     
+## create a folder with name of the day and date / find a better solution here  
 os.chdir("August")
 os.mkdir(today.strftime("%B %d"))
 os.chdir(today.strftime("%B %d"))
-## create a folder with name of the day and date
 
-###import Excel files
-
-###clean up Excel files
+####import Excel files
+## below, consider "~Working files" method analog, if it exists, to avoid hardcoding the path to Working Folder\Working files
+source = r"C:\Users\anton\OneDrive\Desktop\Working folder\Working files" ## this folder is hidden, but components are not
+destination = os.getcwd()
+xl_files = os.listdir(source)
+for file in xl_files:
+    new_xl =  shutil.copy(f"{source}/{file}", destination)
+    splt = os.path.splitext(new_xl)
+    os.rename(new_xl, splt[0] + " " + today.strftime("%B %d") + splt[1])
+    
+## Clean Up files
