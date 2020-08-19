6644c8529a97d031b057c2b2037430513089371f
Anton Goncharenko <63752091+AntonG-89@users.noreply.github.com>
2020-08-18 21:13:13 -0400

    Update office_auto.py
    Re-uploaded code for both functions, since it had to be re-written as start_client() creates its own client list now.

--- office_auto.py
+++ office_auto.py
@@ -1,35 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
 import os 
 from datetime import datetime
 import shutil
+import winshell
+os.chdir(r"C:\Users\anton\OneDrive\Desktop\Working folder")
+base_root = r"C:\Users\anton\OneDrive\Desktop\Working folder"
 
 
-##First function of the day: Create daily folder, populate with worksheets, open the directory for work
-def day_start()
+def day_start():
+    
+    
     today = datetime.now()
     os.chdir(r"C:\Users\anton\OneDrive\Desktop\Working folder")
     ## Creating a new month folder/ accessing current month folder
+    ##insert datetime month format as string instead of August
     month = today.strftime("%B")
     if not os.path.exists(month):
         os.makedirs(month)
-
-    ## create a folder with name of the day and date / find a better solution here  
+    ## create a folder with name of the day and date   
     os.chdir(month)
     os.makedirs(today.strftime("%B %d"), exist_ok = True)
     os.chdir(today.strftime("%B %d"))
-
+    
     ####import Excel files
-    ## below, consider "~Working files" method analog, if it exists, to avoid hardcoding the path to Working Folder\Working files
     source = r"C:\Users\anton\OneDrive\Desktop\Working folder\Working files" ## this folder is hidden, but components are not
     destination = os.getcwd()
     xl_files = os.listdir(source)
     for file in xl_files:
+        ## this error handling is a poor implementation, I want to prevent files from being copied, not renamed
         try:
             new_xl =  shutil.copy(f"{source}/{file}", destination)
             splt = os.path.splitext(new_xl)
             os.rename(new_xl, splt[0] + " " + today.strftime("%B %d") + splt[1])
         except FileExistsError:
-            print("File already exists")
-
-    os.startfile(destination)        
-
-    ## Clean Up files
+            print ("File already exists")
+    
+    os.startfile(destination)
+    ### List of clients for start_client function, need to itirate over list
+       
+def start_client():
+    client_list = os.listdir(r"C:\Users\anton\OneDrive\Desktop\Working folder\Client Base Hidden")
+    openf = input("Enter client's info:Lastname_number :\n")
+    # Bool to find if the folder already exists
+    file_exists = False
+    for client in client_list:
+        if client == openf:
+            ##Here will be creating shortcut to client file in current directory
+            file_exists = True
+            break
+        else:
+            continue
+    # Creating a new folder if new client
+    if file_exists == False:
+         os.mkdir(r"C:\Users\anton\OneDrive\Desktop\Working folder\Client Base Hidden\%s" % openf)
+         
+    # Creating shortcut
+    new_client = r"C:\Users\anton\OneDrive\Desktop\Working folder\Client Base Hidden\%s" % openf    
+    winshell.CreateShortcut(
+            Path = os.path.join(os.getcwd(), "%s.lnk" % openf ), 
+            Target = new_client,
+            Icon = (new_client,0))
